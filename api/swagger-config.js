a95d6c9eb4faf2b1bbccffb5c9a2b46d4fecbff4
matt.h <matt.huang@sjfood.com>
2025-02-24 05:58:25 -0800

    commit

--- api/swagger-config.js
+++ api/swagger-config.js
@@ -3,11 +3,8 @@ const swaggerJSDoc = require('swagger-jsdoc');
 
 const options = {
     definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'GitLab 提交统计 API',
-            version: '1.0.0',
-        },
+        "openapi": "3.0.0",
+        "info": { "title": "GitLab 提交统计 API", "version": "1.0.0" },
         servers: [
             {
                 url: process.env.VERCEL_URL
@@ -15,9 +12,67 @@ const options = {
                     : 'http://localhost:3000',
                 description: process.env.VERCEL ? '生产环境' : '本地开发'
             }
-        ]
+        ],
+        "paths": {
+            "/api/commits":
+            {
+                "get":
+                {
+                    "summary": "统计git提交", "tags": ["统计git提交"],
+                    "parameters":
+                        [{
+                            "in": "query",
+                            "name": "gitlabHost",
+                            "required": true,
+                            "schema": { "type": "string" },
+                            "description": "gitlab服务器地址"
+                        },
+                        {
+                            "in": "query",
+                            "name": "privateToken",
+                            "required": true,
+                            "schema": { "type": "string" },
+                            "description": "gitlab access token"
+                        },
+                        {
+                            "in": "query",
+                            "name": "userIds",
+                            "required": true,
+                            "schema": {
+                                "type": "array",
+                                "items": { "type": "string" }
+                            },
+                            "description": "用户id"
+                        },
+                        {
+                            "in": "query",
+                            "name": "startDate",
+                            "required": true, "schema": { "type": "string", "format": "date" }, "description": "开始时间（格式：YYYY-MM-DD）"
+                        },
+                        {
+                            "in": "query",
+                            "name": "endDate",
+                            "required": true,
+                            "schema": { "type": "string", "format": "date" },
+                            "description": "结束时间（格式：YYYY-MM-DD）"
+                        }],
+                    "responses":
+                    {
+                        "200": {
+                            "description": "成功返回提交统计",
+                            "content": {
+                                "application/json":
+                                {
+                                    "example": { "code": 200, "data": [] }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }, "components": {}, "tags": [{ "name": "统计git提交", "description": "统计某个时间段内某些用户提交的天数和commit个数" }]
     },
-    apis: ['./api/*.js'] // 扫描所有 API 文件
+    apis: [],
 };
 
 module.exports = swaggerJSDoc(options);
